103626c89835c7c40e5ba02578324c698efd6374
irakli <maniaika07@gmail.com>
2026-05-25 03:04:37 +0400

    feat(frontend): render chess game state in board component

--- frontend/src/components/board/ChessBoard.tsx
+++ frontend/src/components/board/ChessBoard.tsx
@@ -1,4 +1,12 @@
+import { useGame } from "../../hooks/useGame"
+
+
 export default function ChessBoard() {
+  const { state, move } = useGame()
+
+  if (!state) return <></>
+
   return (
+    <>FEN: {state.fen}</>
   )
 }
