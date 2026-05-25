cee202507bf489efd9dfff8a5f6abd5bfbdffedb
irakli <maniaika07@gmail.com>
2026-05-26 01:39:59 +0400

    feat: render chessboard from FEN state using Square component

--- frontend/src/components/board/ChessBoard.tsx
+++ frontend/src/components/board/ChessBoard.tsx
@@ -1,12 +1,32 @@
 import { useGame } from "../../hooks/useGame"
+import { parseFEN } from "../../utils/fen"
+import Square from "./Square"
 
 
 export default function ChessBoard() {
   const { state, move } = useGame()
 
-  if (!state) return <></>
+  if (!state) return null
+
+  const board = parseFEN(state)
 
   return (
-    <>FEN: {state.fen}</>
+    <div className="w-fit border">
+      {board.map((row, r) => (
+        <div key={r} className="flex">
+          {row.map((piece, c) => {
+            const isDark = (r + c) % 2 === 1;
+
+            return (
+              <Square
+                key={`${r}-${c}`}
+                isDark={isDark}
+                piece={piece || null}
+              />
+            );
+          })}
+        </div>
+      ))}
+    </div>
   )
 }
