440e198619aa8a7047e24057ec9962dc0050791a
irakli <maniaika07@gmail.com>
2026-05-26 01:46:53 +0400

    chore: update style
    - change to chess.com default colors
    - increase size of pieces

--- frontend/src/components/board/Square.tsx
+++ frontend/src/components/board/Square.tsx
@@ -11,11 +11,11 @@ export default function Square({ isDark, piece }: Props) {
   return (
     <div
       className={`w-[5rem] h-[5rem] flex items-center justify-center ${
-        isDark ? "bg-[#769656]" : "bg-[#eeeed2]"
+        isDark ? "bg-[#739552]" : "bg-[#ebecd0]"
       }`}
     >
       {piece && (
-        <img src={`${PIECES_ASSET_PATH}${piece}.png`} className="w-4/5 h-4/5" />
+        <img src={`${PIECES_ASSET_PATH}${piece}.png`} className="w-8/9 h-8/9" />
       )}
     </div>
   )
