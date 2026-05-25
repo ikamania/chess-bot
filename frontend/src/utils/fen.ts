716868efbaea353451bf199a9682c44498005b45
irakli <maniaika07@gmail.com>
2026-05-26 00:30:43 +0400

    feat: add FEN parser for chess board representation

---
+++ frontend/src/utils/fen.ts
@@ -0,0 +1,23 @@
+export function parseFEN(fen: string): string[][] {
+  const boardPart = fen.split(" ")[0]
+  const boardRows = boardPart.split("/")
+
+  const board: string[][] = []
+  for (const row of boardRows) {
+    const parsedRow: string[] = []
+
+    for (const char of row) {
+      if (isNaN(Number(char))) {
+        parsedRow.push(char)
+      } else {
+        const emptyCount = Number(char)
+        for (let i = 0; i < emptyCount; i++) {
+          parsedRow.push("")
+        }
+      }
+    }
+    board.push(parsedRow)
+  }
+
+  return board
+}
