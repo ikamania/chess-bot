9274cbbc0af9b56ab843d9cee2d67dee77993e58
irakli <maniaika07@gmail.com>
2026-05-28 17:45:30 +0400

    feat: enable draggable piece rendering in square component

--- frontend/src/components/board/Square.tsx
+++ frontend/src/components/board/Square.tsx
@@ -4,19 +4,21 @@ import Piece from "./Piece"
 type Props = {
   isDark: boolean
   piece: string | null
+  onPointerDown: () => void
+  onPointerUp: () => void
 }
 
 
-export default function Square({ isDark, piece }: Props) {
+export default function Square({ isDark, piece, onPointerDown, onPointerUp }: Props) {
   return (
     <div
+      onPointerUp={onPointerUp}
+      onPointerDown={onPointerDown}
       className={`w-[5rem] h-[5rem] flex items-center justify-center ${
         isDark ? "bg-[#739552]" : "bg-[#ebecd0]"
       }`}
     >
-      {piece && (
-        <Piece piece={piece} />
-      )}
+      {piece && <Piece piece={piece} />}
     </div>
   )
 }
