08c393921846e93265a48f2802b4bfec02e16612
irakli <maniaika07@gmail.com>
2026-05-31 20:42:34 +0400

    feat: integrate game state and board into App
    - add useGame hook to App component
    - add guard for missing board state

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,15 @@
 import ChessBoard from "../components/board/ChessBoard"
+import { useGame } from "../hooks/useGame"
 
 
 function App() {
+  const { board } = useGame()
+
+  if (!board) return "No board provided"
+
   return (
     <div className="pt-[2rem] pl-[2rem]">
-      <ChessBoard />
+      <ChessBoard board={board} />
     </div>
   )
 }
